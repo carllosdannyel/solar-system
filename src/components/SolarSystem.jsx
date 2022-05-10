d065506cd7bb42a759fb7996239e2009eda83bfe
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-10 00:28:11 -0300

    importando e renderizando o componente Title no SolarSystem.jsx

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 export default class SolarSystem extends React.Component {
   render() {
-    return <div data-testid="solar-system" />;
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
+    );
   }
 }
